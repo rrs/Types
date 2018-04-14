0f2df3a1f2f277afc3c829b8d7d0af98781044b7
unknown <bobblezor@gmail.com>
2018-04-14 18:46:39 +0100

    added concrete type generator

---
+++ src/Rrs.Types/ConcreteTypeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Rrs.Types
+{
+    public static class ConcreteTypeGenerator<T>
+    {
+        private static readonly IEnumerable<Func<T>> _newFuncs;
+
+        static ConcreteTypeGenerator()
+        {
+            var concreteTypes = typeof(T).ConcreteImplementationsOf();
+            _newFuncs = concreteTypes.Select(t => Expression.Lambda<Func<T>>(Expression.New(t)).Compile());
+        }
+
+        public static IEnumerable<T> Get()
+        {
+            return _newFuncs.Select(o => o());
+        }
+    }
+}
